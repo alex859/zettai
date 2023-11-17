cf2c5b59e7983837d7462e485495186d4ae175c9
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2023-11-17 08:24:51 +0000

    Update main

--- app/src/main/kotlin/app/App.kt
+++ app/src/main/kotlin/app/App.kt
@@ -4,5 +4,12 @@ import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
 fun main() {
-    Zettai(emptyMap()).asServer(Jetty(9090)).start()
+    val items = listOf("write a book", "play piano")
+    val toDoList = ToDoList(ListName("example"), items = items.map(::ToDoItem))
+    val lists = mapOf(User("cicci") to listOf(toDoList))
+    val server = Zettai(lists).asServer(Jetty(PORT))
+    server.start()
+    println("Server started at http://localhost:$PORT/todo/cicci/example")
 }
+
+private const val PORT = 9090
